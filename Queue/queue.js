651eec798de32941ad843227d92357ad33695fe8
Payal Verma <mailpayalverma@gmail.com>
2019-06-04 12:48:10 +0200

    Update queue.js

--- Queue/queue.js
+++ Queue/queue.js
@@ -1,1 +1,14 @@
+class Queue {
+   constructor(){
+     this.array = [];
+   }
+  
+    enqueue(element){
+      return this.array.push(element)
+    }
+  
+    dequeue(){
+       return this.array.shift();
+    }
 
+}
